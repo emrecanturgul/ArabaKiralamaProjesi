9fbfea17c16f0db9a0802c235b70702d3b8930c9
turgu <turgu@EMRE>
2024-03-08 12:15:58 +0300

    Add project files.

---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,56 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal    
+    {
+        List<Car> _cars; 
+        public InMemoryCarDal()
+        {
+           _cars = new List<Car>() {
+                new Car { Id = 1, BrandId = 1, ColorId = 1, Description = "BMW", ModelYear = 2018 },
+                new Car { Id = 2, BrandId = 1, ColorId = 2, Description = "BMW", ModelYear = 2019 },
+                new Car { Id = 3, BrandId = 2, ColorId = 3, Description = "AUDI", ModelYear = 2020 },
+                new Car { Id = 4, BrandId = 2, ColorId = 4, Description = "AUDI", ModelYear = 2021 },
+
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.FirstOrDefault(c => car.Id == c.Id);
+            carToUpdate.Id = car.Id;
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.Description = car.Description;
+            carToUpdate.ColorId = car.ColorId;
+
+
+        }
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+            _cars.Remove(carToDelete);
+
+        }
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+        public List<Car> GetById(int Id)
+        {
+            return _cars.Where(c => c.Id == Id).ToList();
+        }
+
+
+    }
+}
